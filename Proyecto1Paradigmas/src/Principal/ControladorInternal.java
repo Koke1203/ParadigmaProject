ca892712118ec99b5b95d2037303f10ce9e9fd8e
JOAN <joan.corea.aguilar@est.una.ac.cr>
2020-10-07 20:28:05 -0600

    Cambios3

--- Proyecto1Paradigmas/src/Principal/ControladorInternal.java
+++ Proyecto1Paradigmas/src/Principal/ControladorInternal.java
@@ -64,20 +64,13 @@ public class ControladorInternal implements ActionListener {
         //recorremos todo la cadena caracter por caracter y vamos preguntando si es una
         //letra del abecedario
         for (int i = 0; i < formula.length(); i++) {
-            if (Character.isLetter(formula.charAt(i)) || formula.charAt(i) == '¬') {
-                if (formula.charAt(i) == '¬') {
-                    operandos += formula.charAt(i);
-                } else {
-                    if (i > 1) { //si solo hay un digito no es necesario ir a revisar si está repetido
-                        if (esDuplicado(operandos, formula.charAt(i))) {
-                            operandos += formula.charAt(i);
-                        } else if (operandos.charAt(operandos.length() - 1) == '¬') {
-                            System.out.println("Entrando");
-                            operandos = operandos.subSequence(0, operandos.length() - 1).toString();
-                        }
-                    } else {
+            if (Character.isLetter(formula.charAt(i))) {
+                if (i > 1) { //si solo hay un digito no es necesario ir a revisar si está repetido
+                    if (esDuplicado(operandos, formula.charAt(i))) {
                         operandos += formula.charAt(i);
                     }
+                } else {
+                    operandos += formula.charAt(i);
                 }
             }
         }
@@ -88,49 +81,27 @@ public class ControladorInternal implements ActionListener {
         //se llama al metodo para obtener los operandos
         if (esExpresion(formula.replaceAll("\\s", ""))) {
             String formula_division = dividirExpresionOperandos(formula);
-            int j = 0;
-            for (int x = 0; x < formula_division.length(); x++) {
-                if (formula_division.charAt(x) == '¬') {
-                    j++;
-                }
-            }
 
-            int aux = formula_division.length() + 1 - j;
+            int aux = formula_division.length() + 1;
             String variables[] = new String[aux];
-            j = 0; //mantener posiciones del vector
 
             variables[aux - 1] = "f(";
-
-            //ARREGLAR
             for (int i = 0; i < formula_division.length(); i++) {
-                if (formula_division.charAt(i) == '¬') {
-                    variables[j] = "¬";
-                    variables[j] += formula_division.charAt(i + 1);
-                    i++;
-                } else {
-                    variables[j] = formula_division.charAt(i) + "";
-                }
-
+                variables[i] = formula_division.charAt(i) + "";
                 //ir concantenando la ultima columna, donde se evalua la expresion
-                if (j == (aux - 1)) {
+                if (i == aux - 1) {
                     variables[aux - 1] += ")";
                 } else {
-                    if (variables[j].charAt(0) == '¬') {
-                        variables[aux - 1] += "¬" + formula_division.charAt(i) + ", ";
+                    if (i == aux - 2) {
+                        variables[aux - 1] += formula_division.charAt(i);
                     } else {
-                        if (j == (aux - 2)) {
-                            variables[aux - 1] += formula_division.charAt(i);
-                        } else {
-                            variables[aux - 1] += formula_division.charAt(i) + ", ";
-                        }
+                        variables[aux - 1] += formula_division.charAt(i) + ", ";
                     }
                 }
-
-                j++;
             }
+
             variables[aux - 1] += ")";
             String tablaVerdad[][] = obtenerTablaDeVerdad(variables.length - 1);
-            //String matriz[][] = new String[3][3];
 
             //Encabezado de la tabla
             internal.tbVerdad.setModel(
@@ -191,9 +162,10 @@ public class ControladorInternal implements ActionListener {
                         || (Character.isLetter(formula.charAt(i)) && esOperador(formula.charAt(i + 1)) && ("" + formula.charAt(i + 2)).equals("¬"))
                         || (esOperador(formula.charAt(i)) && ("" + formula.charAt(i + 1)).equals("¬") && Character.isLetter(formula.charAt(i + 2)))
                         || (esOperador(formula.charAt(i)) && ("" + formula.charAt(i + 1)).equals("(") && ("" + formula.charAt(i + 2)).equals("¬"))
-                        || (("" + formula.charAt(i)).equals(">") && ("" + formula.charAt(i + 1)).equals("(") && ("" + formula.charAt(i + 2)).equals("¬"))) {
+                        || (("" + formula.charAt(i)).equals(">") && ("" + formula.charAt(i + 1)).equals("(") && ("" + formula.charAt(i + 2)).equals("¬"))
+                        || (("" + formula.charAt(i)).equals("¬") && Character.isLetter(formula.charAt(i + 1)) && ("" + formula.charAt(i + 2)).equals(")"))) {
                     retorno = true;
-
+                    /* */
                 } else {
                     retorno = false;
                     return retorno;
@@ -223,19 +195,9 @@ public class ControladorInternal implements ActionListener {
     public boolean esDuplicado(String formula, char operando) {
         boolean retorno = true;
         for (int i = 0; i < formula.length(); i++) {
-            if (i > 0) { //diferenciamos ¬a y a, se toman como variables diferentes
-                if (formula.charAt(i) == operando && formula.charAt(i - 1) == '¬' && formula.charAt(formula.length() - 1) == '¬') {
-                    retorno = false;
-                    return retorno;
-                } else if (formula.charAt(i) == operando && formula.charAt(i - 1) != '¬' && formula.charAt(formula.length() - 1) != '¬') {
-                    retorno = false;
-                    return retorno;
-                }
-            } else {
-                if (formula.charAt(i) == operando && formula.charAt(formula.length() - 1) != '¬') {
-                    retorno = false;
-                    return retorno;
-                }
+            if (formula.charAt(i) == operando) {
+                retorno = false;
+                return retorno;
             }
         }
         return retorno;
